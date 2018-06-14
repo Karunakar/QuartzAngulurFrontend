69f3b66147530bdcff437d6ff6f5e9120ef4adce
Karuna <revurikarna@gmail.com>
2018-06-14 23:08:47 +0530

    Used styles instead of Style URLS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['h2 { color: green; }']
 })
 export class AppComponent {
   title = 'app';
